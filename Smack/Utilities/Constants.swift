274ec429d9c08f47468b2674a806a9cd43dbe74f
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-14 18:16:25 +0100

    Created another helper.

---
+++ Smack/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  Smack
+//
+//  Created by Raivis on 14/04/2020.
+//  Copyright © 2020 Raivis Olehno. All rights reserved.
+//
+
+import Foundation
+
+
+//Segues
+
+let TO_LOGIN = "toLogin"
