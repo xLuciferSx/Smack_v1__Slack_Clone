0c0f9896cc1f155837f12069118787d1e4ce2d12
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-14 12:18:44 +0100

    Initial Commit

---
+++ Smack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Smack
+//
+//  Created by Raivis on 14/04/2020.
+//  Copyright © 2020 Raivis Olehno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
