8b3de4f4039fdc02a5c4732d600703c83a96dbbf
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-14 18:17:16 +0100

    Created new controller - LoginVC

---
+++ Smack/Controller/LoginVC.swift
@@ -0,0 +1,25 @@
+//
+//  LoginVC.swift
+//  Smack
+//
+//  Created by Raivis on 14/04/2020.
+//  Copyright © 2020 Raivis Olehno. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+  
+    }
+    
+
+    @IBAction func closePressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+}
