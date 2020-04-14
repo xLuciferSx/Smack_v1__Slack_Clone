44a5a93f2bf463946780b1158444c57547b03ef8
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-14 18:52:44 +0100

    Small changes in LoginVC and CreateAccountVC controllers.

---
+++ Smack/Controller/CreateAccountVC.swift
@@ -0,0 +1,27 @@
+//
+//  CreateAccountVC.swift
+//  Smack
+//
+//  Created by Raivis on 14/04/2020.
+//  Copyright © 2020 Raivis Olehno. All rights reserved.
+//
+
+import UIKit
+
+class CreateAccountVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+
+    @IBAction func closedPressed(_ sender: Any) {
+        
+        performSegue(withIdentifier: UNWIND, sender: nil)
+    }
+    
+}
+ 
