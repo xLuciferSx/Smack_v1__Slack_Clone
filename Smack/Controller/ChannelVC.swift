0a5a8a4bba66cf57c8bcf1435cc7c9b83b8988e2
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-14 17:30:41 +0100

    Fixed issue when table view did not assign clear colour by adding custom class to it.

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -9,11 +9,17 @@
 import UIKit
 
 class ChannelVC: UIViewController {
-
+  
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.revealViewController().rearViewRevealWidth = self.view.frame.size.width - 60
-       
+
+      // cell.layer.backgroundColor = UIColor.clear.cgColor
+      // cell.backgroundColor = .clear
+       tableView.layer.backgroundColor = UIColor.clear.cgColor
+       tableView.backgroundColor = .clear
     }
 
 
