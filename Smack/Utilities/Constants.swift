fc25216df09641040a391696c09215b2402da2db
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-16 14:36:13 +0100

    Added extra constants for Web Requests.

--- Smack/Utilities/Constants.swift
+++ Smack/Utilities/Constants.swift
@@ -8,9 +8,21 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success:Bool) -> ()
+
+//URL_CONSTANTS
+
+let BASE_URL = "https://slackclo.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 
 //Segues
 
 let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 let UNWIND = "unwindToChannel"
+
+//User Defaults
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
