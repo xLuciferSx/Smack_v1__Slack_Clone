fe45a863f1ddc5f7aa4e11899fca0ef2ac809ad4
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-16 15:08:03 +0100

    Created RoundedButton class to make the buttons look nicer.

---
+++ Smack/View/RoundedButton.swift
@@ -0,0 +1,31 @@
+//
+//  RoundedButton.swift
+//  Smack
+//
+//  Created by Raivis on 16/04/2020.
+//  Copyright © 2020 Raivis Olehno. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class RoundedButton: UIButton {
+
+    @IBInspectable var cornerRadius: CGFloat = 3.0 {
+        didSet {
+            self.layer.cornerRadius = cornerRadius
+        }
+    }
+    override func awakeFromNib() {
+        self.setupView()
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        self.setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = cornerRadius
+    }
+}
