2f39cbcd256ba3cf06ac211184fa4f98da84faef
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-17 16:08:06 +0100

    Created avatarPicker function. Which will allow to choose various avatars .

---
+++ Smack/Controller/AvatarPickerVC.swift
@@ -0,0 +1,51 @@
+//
+//  AvatarPickerVC.swift
+//  Smack
+//
+//  Created by Raivis on 17/04/2020.
+//  Copyright © 2020 Raivis Olehno. All rights reserved.
+//
+
+import UIKit
+
+class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+//Outlets
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        collectionView.delegate = self
+        collectionView.dataSource = self
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        if let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "avatarCell", for: indexPath) as? AvatarCell {
+            return cell
+        }
+        return AvatarCell()
+    }
+    
+    func numberOfSections(in collectionView: UICollectionView) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 28
+    }
+    
+    @IBAction func segmentControlChanged(_ sender: Any) {
+        
+        
+    }
+    
+    
+    
+    @IBAction func backPressed(_ sender: Any) {
+        
+        dismiss(animated: true, completion: nil)
+    }
+}
