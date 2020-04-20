7373f798217e3ffa2b5928ff62d204dd92495988
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-20 13:23:13 +0100

    Created a new instance for profileVC which will display profile information in modal screen.

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -36,7 +36,16 @@ class ChannelVC: UIViewController {
     }
     
     @IBAction func loginBtnPressed(_ sender: Any) {
-        performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        if AuthService.instance.isLoggedIn {
+            //Show profile page
+            let profile = ProfileVC()
+            profile.modalPresentationStyle = .custom
+            present(profile, animated: true, completion: nil)
+        } else {
+             performSegue(withIdentifier: TO_LOGIN, sender: nil)
+        }
+        
+       
     }
     
     @IBAction func prepareForUnwind (segue: UIStoryboardSegue) {
