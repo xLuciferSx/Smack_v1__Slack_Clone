d95e3aabdecb33bfb81e8802b9866d266702ceb6
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-20 11:40:38 +0100

    User will be able to choose background color for avatar image. After "Create Account" button has been pressed , user will see activity indicator . If everything is fine then user will be sent to home screen.

--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -18,15 +18,21 @@ class CreateAccountVC: UIViewController {
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var userImage: UIImageView!
+    @IBOutlet weak var spinner: UIActivityIndicatorView!
+    
+    
     
     //Variables
     
-   var avatarName = "profileDefault"
+    var avatarName = "profileDefault"
     var avatarColor = "[0.5 ,0.5 ,0.5, 1]"
+    var bgColor : UIColor?
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
         hideKeyboardWhenTappedAround()
+        setupView()
         
     }
     
@@ -34,6 +40,9 @@ class CreateAccountVC: UIViewController {
         if UserDataService.instance.avatarName != "" {
             userImage.image = UIImage(named: UserDataService.instance.avatarName)
             avatarName = UserDataService.instance.avatarName
+            if avatarName.contains("light") && bgColor == nil {
+                userImage.backgroundColor = UIColor.lightGray
+            }
         }
     }
  
@@ -43,6 +52,8 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func createAccountPressed(_ sender: Any) {
+        spinner.isHidden = false
+        spinner.startAnimating()
         guard let name = userNameText.text , userNameText.text != "" else { return }
         guard let email = emailTextField.text , emailTextField.text != "" else { return }
         guard let pass = passwordTextField.text , passwordTextField.text != "" else { return }
@@ -53,8 +64,10 @@ class CreateAccountVC: UIViewController {
                     if success {
                         AuthService.instance.createUser(name: name, email: email, avatarName: self.avatarName, avatarColor: self.avatarColor, completion: { (success) in
                             if success {
-                                print(UserDataService.instance.name, UserDataService.instance.avatarName)
+                                self.spinner.isHidden = true
+                                self.spinner.stopAnimating()
                                 self.performSegue(withIdentifier: UNWIND, sender: nil)
+                                NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
                             }
                         })
                     }
@@ -71,10 +84,23 @@ class CreateAccountVC: UIViewController {
     }
     
     @IBAction func pickBackgroundColorPressed(_ sender: Any) {
+        
+        let r = CGFloat(arc4random_uniform(255)) / 255
+        let g = CGFloat(arc4random_uniform(255)) / 255
+        let b = CGFloat(arc4random_uniform(255)) / 255
+        
+        bgColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+        UIView.animate(withDuration: 0.2) {
+            self.userImage.backgroundColor = self.bgColor
+        }
     }
   
-    
-
+    func setupView() {
+        spinner.isHidden = true
+        userNameText.attributedPlaceholder = NSAttributedString(string: "username", attributes: [NSAttributedString.Key.foregroundColor : smackPurplePlaceHolder])
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "email", attributes: [NSAttributedString.Key.foregroundColor : smackPurplePlaceHolder])
+        passwordTextField.attributedPlaceholder = NSAttributedString(string: "password", attributes: [NSAttributedString.Key.foregroundColor : smackPurplePlaceHolder])
+    }
 }
  
 extension  UIViewController {
