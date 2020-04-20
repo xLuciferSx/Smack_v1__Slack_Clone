b74bc7712c63f1b6e3e07847a763c9e9cb1829f9
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-20 11:41:48 +0100

    All the images are going to be rounded.

---
+++ Smack/View/CircleImage.swift
@@ -0,0 +1,27 @@
+//
+//  CircleImage.swift
+//  Smack
+//
+//  Created by Raivis on 20/04/2020.
+//  Copyright © 2020 Raivis Olehno. All rights reserved.
+//
+
+import UIKit
+
+@IBDesignable
+class CircleImage: UIImageView {
+
+    override func awakeFromNib() {
+        setupView()
+    }
+    
+    func setupView() {
+        self.layer.cornerRadius = self.frame.width / 2
+        self.clipsToBounds = true
+    }
+    
+    override func prepareForInterfaceBuilder() {
+        super.prepareForInterfaceBuilder()
+        setupView()
+    }
+}
