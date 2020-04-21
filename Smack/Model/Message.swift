3de2bb0377b3ad8c2271f8c6d28d201859823e1a
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-21 17:07:17 +0100

    Created a Model for Messages

---
+++ Smack/Model/Message.swift
@@ -0,0 +1,19 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by Raivis on 21/04/2020.
+//  Copyright © 2020 Jonny B. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message : String!
+    public private(set) var userName : String!
+    public private(set) var channelId : String!
+    public private(set) var userAvatar : String!
+    public private(set) var userAvatarColor : String!
+    public private(set) var id : String!
+    public private(set) var timeStamp : String!
+}
