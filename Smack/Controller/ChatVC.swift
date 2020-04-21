e47ec73fde0f200cc348f42fc0a91e07b66f86ab
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-21 16:15:29 +0100

    Small improvements in ChatVC. Now selected channel will display in top of the screen.

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -12,25 +12,45 @@ class ChatVC: UIViewController {
     
     // Outlets
     @IBOutlet weak var menuBtn: UIButton!
+    @IBOutlet weak var channelNameLbl: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
         self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
-        
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIF_CHANNELS_SELECTED, object: nil)
         if AuthService.instance.isLoggedIn {
             AuthService.instance.findUserByEmail(completion: { (success) in
                 NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
             })
         }
-        if AuthService.instance.authToken != nil {
-            MessageService.instance.findAllChannel { (success) in
-                       
-                   }
-        }
-       
     }
-
+    
+    
+    @objc func userDataDidChange(_ notif: Notification) {
+        if AuthService.instance.isLoggedIn {
+            //get channels
+            onLoginGetMessages()
+        } else {
+            channelNameLbl.text = "Please Log In"
+        }
+             }
+    func onLoginGetMessages(){
+        MessageService.instance.findAllChannel { (success) in
+            if success {
+            
+            }
+        }
+    }
+    
+    @objc func channelSelected(_ notif:Notification) {
+        updateWithChannell()
+    }
+    
+    func updateWithChannell(){
+        let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
+        channelNameLbl.text = "#\(channelName)"
+        }
 }
