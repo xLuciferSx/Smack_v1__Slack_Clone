32c57a142ab6949fc28a58eb2993fa909e148c1a
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-21 17:45:31 +0100

    Created an instance for addMessage. Function will send message to the databse.

--- Smack/Services/SocketService.swift
+++ Smack/Services/SocketService.swift
@@ -44,15 +44,11 @@ class SocketService: NSObject {
             completion(true)
         }
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
+
+    func addMessage(messageBody: String, userId: String, channelId: String, completion: @escaping CompletionHandler){
+        let user = UserDataService.instance
+        socket.emit("newMessage", messageBody, userId, channelId, user.name, user.avatarName, user.avatarColor)
+        completion(true)
+    }
     
 }
