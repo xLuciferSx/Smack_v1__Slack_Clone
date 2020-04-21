e65af5ad9edd6f30ee40c0c5db81f761e2d88962
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-21 18:27:09 +0100

    Created a function which will display all the messages from the database

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -8,95 +8,131 @@
 
 import UIKit
 
-class ChatVC: UIViewController {
+class ChatVC: UIViewController , UITableViewDelegate, UITableViewDataSource{
     
     // Outlets
     @IBOutlet weak var menuBtn: UIButton!
     @IBOutlet weak var channelNameLbl: UILabel!
     @IBOutlet weak var messageTextBox: UITextField!
+    @IBOutlet weak var tableView: UITableView!
     
     
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.bindToKeyboard()
-        let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
-        view.addGestureRecognizer(tap)
-        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
-        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
-        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
-        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIF_CHANNELS_SELECTED, object: nil)
-        if AuthService.instance.isLoggedIn {
-            AuthService.instance.findUserByEmail(completion: { (success) in
-                NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
-            })
-        }
-    }
-    
-    @objc func handleTap(){
-        view.endEditing(true)
-    }
-    
-    @objc func userDataDidChange(_ notif: Notification) {
-        if AuthService.instance.isLoggedIn {
-            //get channels
-            onLoginGetMessages()
-        } else {
-            channelNameLbl.text = "Please Log In"
-        }
-             }
-    
-    
-    func onLoginGetMessages(){
-        MessageService.instance.findAllChannel { (success) in
-            if success {
-                if MessageService.instance.channels.count > 0 {
-                    MessageService.instance.selectedChannel = MessageService.instance.channels[0]
-                    self.updateWithChannell()
-                } else {
-                    self.channelNameLbl.text = "No channels yet!"
-                }
-            }
-        }
-    }
-    
-    @objc func channelSelected(_ notif:Notification) {
-        updateWithChannell()
-    }
-    
-    func updateWithChannell(){
-        let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
-        channelNameLbl.text = "#\(channelName)"
-        getMessages()
-        }
-    
-    @IBAction func sendMessagePressed(_ sender: Any) {
-        if AuthService.instance.isLoggedIn {
-            guard let channelId = MessageService.instance.selectedChannel?.id else {return}
-            guard let message = messageTextBox.text else {return}
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            view.bindToKeyboard()
+            tableView.delegate = self
+            tableView.dataSource = self
+            tableView.estimatedRowHeight = 80
+            tableView.rowHeight = UITableViewAutomaticDimension
             
-            SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId) { (success) in
+            let tap = UITapGestureRecognizer(target: self, action: #selector(ChatVC.handleTap))
+            view.addGestureRecognizer(tap)
+            menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+            self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+            self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+            
+            NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+            NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIF_CHANNELS_SELECTED, object: nil)
+            
+            if AuthService.instance.isLoggedIn {
+                AuthService.instance.findUserByEmail(completion: { (success) in
+                    NotificationCenter.default.post(name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
+                })
+            }
+        }
+        
+        @objc func userDataDidChange(_ notif: Notification) {
+            if AuthService.instance.isLoggedIn {
+                onLoginGetMessages()
+            } else {
+                channelNameLbl.text = "Please Log In"
+            }
+        }
+        
+        @objc func channelSelected(_ notif: Notification) {
+            updateWithChannel()
+        }
+        
+        @objc func handleTap() {
+            view.endEditing(true)
+        }
+        
+        func updateWithChannel() {
+            let channelName = MessageService.instance.selectedChannel?.channelTitle ?? ""
+            channelNameLbl.text = "#\(channelName)"
+            getMessages()
+        }
+        
+            @IBAction func sendMessagePressed(_ sender: Any) {
+                if AuthService.instance.isLoggedIn {
+                    guard let channelId = MessageService.instance.selectedChannel?.id else {return}
+                    guard let message = messageTextBox.text else {return}
+        
+                    SocketService.instance.addMessage(messageBody: message, userId: UserDataService.instance.id, channelId: channelId) { (success) in
+                        if success {
+                            self.messageTextBox.text = ""
+                            self.messageTextBox.resignFirstResponder()
+        
+                        }
+                    }
+                }
+        
+            }
+        
+        
+        func onLoginGetMessages() {
+            MessageService.instance.findAllChannel { (success) in
                 if success {
-                    self.messageTextBox.text = ""
-                    self.messageTextBox.resignFirstResponder()
-                    
+                    if MessageService.instance.channels.count > 0 {
+                        MessageService.instance.selectedChannel = MessageService.instance.channels[0]
+                        self.updateWithChannel()
+                    } else {
+                        self.channelNameLbl.text = "No channels yet!"
+                    }
                 }
             }
         }
         
-    }
-    
-}
-
-
-func getMessages(){
-    
-    guard let channelId = MessageService.instance.selectedChannel?.id else { return }
-    MessageService.instance.findAllmessagesForChannel(channelId: channelId) { (success) in
+        func getMessages() {
+            guard let channelId = MessageService.instance.selectedChannel?.id else { return }
+            MessageService.instance.findAllmessagesForChannel(channelId: channelId) { (success) in
+                if success {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+        
+        func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            if let cell = tableView.dequeueReusableCell(withIdentifier: "messageCell", for: indexPath) as? MessageCell {
+                let message = MessageService.instance.messages[indexPath.row]
+                cell.configureCell(message: message)
+                return cell
+            } else {
+                return UITableViewCell()
+            }
+        }
+        
+        func numberOfSections(in tableView: UITableView) -> Int {
+            return 1
+        }
+        
+        func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            return MessageService.instance.messages.count
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
         
     }
-    
-}
+
+
 
 
