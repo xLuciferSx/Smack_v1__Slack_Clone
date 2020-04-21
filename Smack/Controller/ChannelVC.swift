09b55b2ef988bb6c2059959ffb3925c26d7be7ff
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-21 10:44:50 +0100

    Create channel button is currently active and will present modal screen.

--- Smack/Controller/ChannelVC.swift
+++ Smack/Controller/ChannelVC.swift
@@ -83,4 +83,10 @@ class ChannelVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
     }
     
     
+    @IBAction func addChannelPressed(_ sender: Any) {
+        
+        let addChannel = AddChannelVC()
+        addChannel.modalPresentationStyle = .custom
+        present(addChannel, animated: true, completion: nil)
+    }
 }
