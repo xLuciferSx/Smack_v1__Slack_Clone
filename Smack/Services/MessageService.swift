ec0b29c5a0d3b932590f10af0d20dad0d490fcf9
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-21 16:15:59 +0100

    After user is logged out . All channels will be cleared from Hamburger menu.

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -15,6 +15,7 @@ class MessageService {
     static let instance = MessageService()
     
     var channels = [Channel]()
+    var selectedChannel : Channel?
     
     func findAllChannel(completion: @escaping CompletionHandler) {
         AF.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
@@ -29,18 +30,19 @@ class MessageService {
                         let channel = Channel(channelTitle: name, channelDescription: channelDescription, id: id)
                         self.channels.append(channel)
                     }
-                    print(self.channels[0].channelTitle)
+                    NotificationCenter.default.post(name: NOTIF_CHANNELS_LOADED, object: nil)
                     completion(true)
-                }
-                
-                
-                
+                }    
             } else {
                 completion(false)
                 debugPrint(response.error as Any)
             }
         }
         
+    }
+    
+    func clearChannels(){
+        channels.removeAll()
     }
     
 }
