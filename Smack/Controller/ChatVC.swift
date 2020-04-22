3a97e9c2b3b4a6a0f988c8f7e7c96aeb0f1d49d6
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-22 16:49:56 +0100

    Created an option that people will see if someone is typing.

--- Smack/Controller/ChatVC.swift
+++ Smack/Controller/ChatVC.swift
@@ -47,6 +47,34 @@ class ChatVC: UIViewController , UITableViewDelegate, UITableViewDataSource{
                     self.tableView.scrollToRow(at: endIndex, at: .bottom, animated: false)
                 }
             }
+            
+            SocketService.instance.getTypingUsers{ (typingUsers) in
+                guard let channelId = MessageService.instance.selectedChannel?.id else {return}
+                var names = ""
+                var numberOfTypers = 0
+                
+                for (typingUser, channel) in typingUsers {
+                    if typingUser != UserDataService.instance.name && channel == channelId {
+                        if names == "" {
+                            names = typingUser
+                        } else {
+                            names = "\(names), \(typingUser)"
+                        }
+                        numberOfTypers += 1
+                    }
+                }
+                
+                if numberOfTypers > 0 && AuthService.instance.isLoggedIn == true {
+                    var verb = "is"
+                    if numberOfTypers > 1 {
+                        verb = "are"
+                    }
+                    self.typingUsersLabel.text = "\(names) \(verb) typing a message"
+                } else {
+                    self.typingUsersLabel.text = ""
+                }
+    }
+            
             NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.userDataDidChange(_:)), name: NOTIF_USER_DATA_DID_CHANGE, object: nil)
             NotificationCenter.default.addObserver(self, selector: #selector(ChatVC.channelSelected(_:)), name: NOTIF_CHANNELS_SELECTED, object: nil)
             
@@ -84,13 +112,15 @@ class ChatVC: UIViewController , UITableViewDelegate, UITableViewDataSource{
         
     
     @IBAction func messageBoxEditing(_ sender: Any) {
-        
+        guard let channelId = MessageService.instance.selectedChannel?.id else { return }
         if messageTextBox.text == "" {
             isTyping = false
             sendButton.isHidden = true
+            SocketService.instance.socket.emit("stopType", UserDataService.instance.name, channelId )
         } else {
             if isTyping == false {
                 sendButton.isHidden = false
+                SocketService.instance.socket.emit("startType", UserDataService.instance.name, channelId)
             }
             isTyping = true
         }
@@ -105,6 +135,7 @@ class ChatVC: UIViewController , UITableViewDelegate, UITableViewDataSource{
                         if success {
                             self.messageTextBox.text = ""
                             self.messageTextBox.resignFirstResponder()
+                            SocketService.instance.socket.emit("stopType", UserDataService.instance.name, channelId)
         
                         }
                     }
