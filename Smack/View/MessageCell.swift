2b853e498b7f2f926e76fcc810577e79dc9e99a5
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-22 18:13:00 +0100

    Created a formatter which will format date from default timeStamp to apple ISO8601 Date Format.

--- Smack/View/MessageCell.swift
+++ Smack/View/MessageCell.swift
@@ -26,6 +26,22 @@ class MessageCell: UITableViewCell {
         userNameLabel.text = message.userName
         userImage.image = UIImage(named: message.userAvatar)
         userImage.backgroundColor = UserDataService.instance.returnUIColor(components: message.userAvatarColor)
+        
+        
+        guard var isoDate = message.timeStamp else {return}
+        let end = isoDate.index(isoDate.endIndex, offsetBy: -5)
+        isoDate = isoDate.substring(to: end)
+        
+        let isoFormatter = ISO8601DateFormatter()
+        let chatDate = isoFormatter.date(from: isoDate.appending("Z"))
+        
+        let newFormatter = DateFormatter()
+        newFormatter.dateFormat = "MMM d, h:mm a"
+        
+        if let finalDate = chatDate {
+            let finalDate = newFormatter.string(from: finalDate)
+            timeStapLabel.text = finalDate
+        }
     }
 
 }
