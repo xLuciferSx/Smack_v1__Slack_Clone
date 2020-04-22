72a700244b6b168d3635de78980eb0a6511c842a
Raivis Olehno <raivis.olehno@outlook.com>
2020-04-22 18:14:11 +0100

    Added new variable unreadChannels

--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -16,7 +16,9 @@ class MessageService {
     
     var channels = [Channel]()
     var messages = [Message]()
+    var unreadChannels = [String]()
     var selectedChannel : Channel?
+   
     
     func findAllChannel(completion: @escaping CompletionHandler) {
         AF.request(URL_GET_CHANNELS, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
